53e77614ac3beb0268604ec5a64dc5f3e08f322f
Evan Chen <ebzlo@ebzlo.com>
2012-11-30 04:10:04 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taurusdemo::Application.config.secret_token = '90ed351e7c8958151b012f0194b3f22d3bbdb30fb42ab4c205048ebdbf027a9ffdea6d9392dbd6eff737a1fcb35ba8f2df3701f852b358e315c98e0a23ed4877'
